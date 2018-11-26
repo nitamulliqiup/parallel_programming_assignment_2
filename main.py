4196bb653c2bbb5047abafd6b9fc6477b9f851b0
= <nita.mulliqi@studentet.uni-pr.edu>
2018-11-26 22:05:58 +0100

    Added choice to repeat calculations

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import multiprocessing
+import datetime
 from prime_numbers import PrimeNumbers
 
 prime_object = PrimeNumbers()
 
 primes_till = 100000000
-primes = multiprocessing.Array('b',primes_till)
 counter = 0
 print('Parallel Programming Assignment 2')
 print('*********************************')
@@ -18,17 +18,34 @@ def increase_counter(number):
 
 
 running = True
+table = []
 while running:
+    primes = multiprocessing.Array('b', primes_till)
     primes_till = eval(input("Enter max number: "))
     processors_count = multiprocessing.cpu_count()
     chosen_processors = eval(input(f'Chose number of processors from 1 to {processors_count}: '))
+    if chosen_processors not in range(1, processors_count + 1):
+        print(f'You chose {chosen_processors}, that is not included in the expected range! We are proceeding with the maximum power.')
+        chosen_processors = processors_count
+
     print(f'Calculating with {chosen_processors} processors...')
-    pool = multiprocessing.Pool(processes=5)
+    pool = multiprocessing.Pool(processes=chosen_processors)
+    start_time = datetime.datetime.now()
     pool.map(increase_counter, range(1, primes_till + 1))
     prime_count = primes[:].count(1)
+    end_time = datetime.datetime.now()
     print(f'{prime_count} prime numbers found from {1} till {primes_till}')
     end_response = input("Do you want to continue? (Y/N): ")
+    diff = end_time - start_time
+    mins = int((diff.seconds // 60) % 60)
+    result = {"Number": primes_till, "Processors": chosen_processors, "Start Time": start_time.strftime("%H:%M:%S"), "End Time": end_time.strftime("%H:%M:%S"), "Primes found": prime_count, "Duration in minutes":mins, "Duration in seconds": diff.seconds}
+    table.append(result)
     if end_response not in ('Y', 'y'):
         running = False
 
 print('*********************************')
+print('Displaying Results')
+for row in table:
+    print(row)
+
+
