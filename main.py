599f89862004dcd6d7e567c6a4f2a9baef9b3ecc
Rilind <rilind.mehmeti@studentet.uni-pr.edu>
2018-11-26 23:21:05 +0100

    Small adjustments

--- main.py
+++ main.py
@@ -24,24 +24,28 @@ while running:
     primes_till = eval(input("Enter max number: "))
     processors_count = multiprocessing.cpu_count()
     chosen_processors = eval(input(f'Chose number of processors from 1 to {processors_count}: '))
+
     if chosen_processors not in range(1, processors_count + 1):
         print(f'You chose {chosen_processors}, '
               f'that is not included in the expected range! We are proceeding with the maximum power.')
         chosen_processors = processors_count
 
     print(f'Calculating with {chosen_processors} processors...')
+
     pool = multiprocessing.Pool(processes=chosen_processors)
     start_time = datetime.datetime.now()
     pool.map(increase_counter, range(1, primes_till + 1))
     prime_count = primes[:].count(1)
     end_time = datetime.datetime.now()
+
     print(f'{prime_count} prime numbers found from {1} till {primes_till}')
     end_response = input("Do you want to continue? (Y/N): ")
+
     diff = end_time - start_time
-    mins = int((diff.seconds // 60) % 60)
+    minutes = int((diff.seconds // 60) % 60)
     result = {"Number": primes_till, "Processors": chosen_processors, "Start Time": start_time.strftime("%H:%M:%S"),
               "End Time": end_time.strftime("%H:%M:%S"), "Primes found": prime_count,
-              "Duration in minutes":mins, "Duration in seconds": diff.seconds}
+              "Duration in minutes":minutes, "Duration in seconds": diff.seconds}
     table.append(result)
     if end_response not in ('Y', 'y'):
         running = False
